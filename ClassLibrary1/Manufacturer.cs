73f1657ba82afe386942a903da9cc8415b0c05ed
Zhevgan2025 <dubichul@gmail.com>
2026-03-24 00:06:07 +0200

    add ClassLibrary1 project

---
+++ ClassLibrary1/Manufacturer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Manufacturer
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        private bool IsAChildCompany { get; set; }
+
+        public Manufacturer()
+        {
+        }
+
+        public void Create(string name, string address, bool isAChildCompany)
+        {
+            Name = name;
+            Address = address;
+            IsAChildCompany = isAChildCompany;
+        }
+
+        public void PrintObject()
+        {
+            System.Console.WriteLine($"Name: {Name}, Address: {Address}, IsAChildCompany: {IsAChildCompany}");
+        }
+    }
+}
---
+++ ClassLibrary1/Pc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class PC
+    {
+        private int ID { get; set; }
+        public string Model { get; set; }
+        public string SerialNumber { get; set; }
+        public string PCType { get; set; }
+
+        public PC()
+        {
+        }
+
+        public void Create(int id, string model, string serialNumber, string pcType)
+        {
+            ID = id;
+            Model = model;
+            SerialNumber = serialNumber;
+            PCType = pcType;
+        }
+
+        public void PrintObject()
+        {
+            System.Console.WriteLine($"ID: {ID}, Model: {Model}, SerialNumber: {SerialNumber}, PCType: {PCType}");
+        }
+    }
+}
